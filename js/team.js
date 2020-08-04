b45f76ec03181c6bfaf079be400084f2df60c039
liyanzhu <296652033@qq.com>
2020-08-05 07:13:25 +0800

    second

--- js/team.js
+++ js/team.js
@@ -1,7 +1,13 @@
+var tecon = document.querySelector('.tecon');
+console.log(tecon);
 var docters = document.querySelectorAll('.techange div')
 var items = document.querySelectorAll('.tecon div');
 for (let i = 0; i < docters.length; i++) {
   docters[i].onmouseover = function () {
+    // animate(tecon, {
+    //   "margin-left": -i * 900
+    // }, 500);
+
     for (let j = 0; j < docters.length; j++) {
       items[j].style.display = 'none';
       docters[j].style.backgroundColor = '#1e82e8';
