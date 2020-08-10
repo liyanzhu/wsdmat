2d741a8a19e10a2a885308d7f5dcda09c76d7cf9
liyanzhu <296652033@qq.com>
2020-08-10 22:34:05 +0800

    again

--- js/header.js
+++ js/header.js
@@ -26,3 +26,63 @@ pro.onmouseout = function () {
 displ.onmouseout = function () {
   displ.style.display = 'none';
 }
+
+//侧边固定
+var lis = document.querySelectorAll('.rightfix li');
+console.log(lis);
+lis[lis.length-1].onclick = function () {
+  animate2(document.documentElement, {
+    scrollTop: 0
+  }, 500)
+  // window.scrollTo(0, 0)
+}
+
+//轮播图
+var swiper = document.querySelector('.swiper')
+var box = document.querySelector('.swiper .box');
+var swipers = document.querySelectorAll('.swiper .box img')
+var winW = parseInt(getComputedStyle(swiper, null).width);
+var btns = document.querySelectorAll('.swiper .btns li')
+var num = 0;
+function move() {
+    num++;
+    if (num > swipers.length - 2) {
+      animate2(box, {
+        "margin-left": -num * winW
+      }, 500, Tween.Linear, () =>{
+        box.style.marginLeft = 0;
+      });
+      num = 0;
+    } else {
+      animate2(box, {
+        "margin-left": -num * winW
+      }, 500)
+    }
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].style.backgroundColor = '#d0d0d0';
+    }
+    btns[num].style.backgroundColor = '#555';
+}
+var t = setInterval(move, 3000);
+
+//按钮点击
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = function () {
+    num = i;
+    animate2(box, {
+      "margin-left": -num * winW
+    }, 500);
+    for (let j = 0; j < btns.length; j++) {
+      btns[j].style.backgroundColor = '#d0d0d0';
+    }
+    btns[num].style.backgroundColor = '#555';
+  }
+}
+
+//鼠标的移入移出
+swiper.onmouseover = function () {
+  clearInterval(t)
+}
+swiper.onmouseout = function () {
+  t = setInterval(move, 3000);
+}
--- js/mobile/index.js
+++ js/mobile/index.js
@@ -1,11 +1,30 @@
 var shouye = document.querySelector('.header .shouye .shou');
 var daohang = document.querySelector('.header .shouye .daohang');
-var aboutus = document.querySelector('.header .shouye .daohang .aboutus');
-var product = document.querySelector('.header .shouye .daohang .proinstro');
 
 shouye.ontouchend = function () {
+  console.log('touched');
   // daohang.style.top = 0;
   animate(daohang, {
     top: 0
   }, 500)
 }
+var swipers = document.querySelectorAll('.swi .swiper');
+var swip = document.querySelector('.swi .swip');
+var num = 0;
+setInterval(() =>{
+  num++;
+  if (num > swipers.length - 2) {
+    animate(swip, {
+      "margin-left": -num * 375
+    }, 2000, Tween.Linear, function () {
+      swip.style.marginLeft = 0;
+    })
+    num = 0;
+  }else {
+    animate(swip, {
+      "margin-left": -num * 375
+    }, 500);
+  }
+
+
+}, 3000)
